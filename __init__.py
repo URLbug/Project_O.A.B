002316b43b36c3039ed5bf98e644e646bdeab583
URLbug <gemslove2@gmail.com>
2023-10-13 23:24:38 +0300

    Project-O.A.B

--- __init__.py
+++ __init__.py
@@ -2,6 +2,9 @@ import os
 
 import tensorflow as tf
 
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 dirs = ['battle',
         'battle_end', 
         'battle_load_color',
--- main.py
+++ main.py
@@ -17,26 +17,54 @@ def press_key(index):
         keyboard.press(keys[index])
         keyboard.release(keys[index])
 
+def seting():
+    print('Настройка клавишь')
+    print(f"Начать уровень - {keys['main']}\nНачать уровень 2 (команда) - {keys['team']}\nЗакончить уровень - {keys['end']}")
+
+    inp = input('Редактироваь? (y/n) ')
+
+    if inp not in ['n', 'y']:
+        inp = input('Редактироваь? (y/n) ')
+    
+    if inp == 'y':
+        keys['main'] = input('укажите новую клавишу для Начать уровень ')
+        keys['team'] = input('укажите новую клавишу для Начать уровень 2 (команда) ') 
+        keys['end'] = input('укажите новую клавишу для Закончить уровень ') 
+
+
+
 def main():
     model = tf.keras.models.load_model('./model/beta_resnet.h5', custom_objects={'Residual': Residual})
     
-    inp = int(input('Сколько раз вы хотите пройти уровень? - '))
-
-    lv = 0
-
     while True:
-        time.sleep(5)
-        pyautogui.screenshot('./screen.png')
-
-        index = resnet_ark('./screen.png', model)
+        print('Настройка - 1\nСтарт - 2\nВыйти - 3')
         
-        press_key(index)
+        inp = input('-> ')
 
-        if index == 'main':
-            if lv == inp:
-                break
-            
-            lv += 1
+        if inp not in ['1', '2', '3']:
+            inp = input('-> ')
+        elif inp == '1':
+            seting()
+        elif inp == '2':
+            inp = int(input('Сколько раз вы хотите пройти уровень? '))
+
+            lv = 0
+
+            while True:
+                time.sleep(5)
+                pyautogui.screenshot('./screen.png')
+
+                index = resnet_ark('./screen.png', model)
+                
+                press_key(index)
+
+                if index == 'main':
+                    if lv == inp:
+                        break
+                    
+                    lv += 1
+        else:
+            exit()
 
 if __name__ == '__main__':
     main()
